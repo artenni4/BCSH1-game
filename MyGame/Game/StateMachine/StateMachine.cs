e2b4ec3e2ed7812309dfe6e91fc6d1aa868199f3
Artem Puzik <artenni4@gmail.com>
2023-02-26 22:28:49 +0100

    refactor FSM

--- MyGame/Game/StateMachine/StateMachine.cs
+++ MyGame/Game/StateMachine/StateMachine.cs
@@ -10,7 +10,7 @@ namespace MyGame.Game.StateMachine
     /// </summary>
     internal class StateMachine<TState, TTrigger>
     {
-        private readonly Dictionary<TState, ICollection<KeyValuePair<TTrigger, TState>>> _triggerTransitions = new();
+        private readonly Dictionary<TState, Dictionary<TTrigger, TState>> _triggerTransitions = new();
         private readonly Dictionary<TState, KeyValuePair<TimeSpan, TState>> _timeTransitions = new();
         private readonly Dictionary<TState, Action<TransitionInfo>> _stateOnEnter = new(); // TODO: allow list of actions on enter (make list)
         private readonly Dictionary<TState, Action<TState, TimeSpan>> _stateOnUpdate = new();
@@ -24,7 +24,7 @@ namespace MyGame.Game.StateMachine
         { 
             get => _state; 
             private set 
-            { 
+            {
                 var prevState = _state;
                 _state = value;
                 stateSet = TimeSpan.Zero;
@@ -47,20 +47,9 @@ namespace MyGame.Game.StateMachine
         /// <exception cref="Exception">Exception thrown if trigger is set to change state to more than one defined state</exception>
         public bool Trigger(TTrigger trigger)
         {
-            var prevState = State;
-            bool changed = false; // ensure only one transition is bound to the trigger
-            if (_triggerTransitions.TryGetValue(State, out var transitions))
+            if (_triggerTransitions.TryGetValue(State, out var transitions) && transitions.TryGetValue(trigger, out var nextState))
             {
-                foreach (var state in transitions)
-                {
-                    if (Equals(state.Key, trigger))
-                    {
-                        if (changed) throw new Exception("More than one transition is not allowed for one trigger");
-
-                        State = state.Value;
-                        changed = true;
-                    }
-                }
+                State = nextState;
                 return true;
             }
             return false;
@@ -76,7 +65,6 @@ namespace MyGame.Game.StateMachine
             stateSet += timeSpan;
             if (_timeTransitions.TryGetValue(State, out var transition) && stateSet >= transition.Key)
             {
-                var prevState = State;
                 State = transition.Value;
             }
 
@@ -138,7 +126,7 @@ namespace MyGame.Game.StateMachine
 
             public Builder DeadEnd()
             {
-                _machine._triggerTransitions.Add(_currentState, new List<KeyValuePair<TTrigger, TState>>());
+                _machine._triggerTransitions.Add(_currentState, new Dictionary<TTrigger, TState>());
                 return this;
             }
 
@@ -182,11 +170,11 @@ namespace MyGame.Game.StateMachine
             {
                 if (_machine._triggerTransitions.TryGetValue(_currentState, out var collection))
                 {
-                    collection.Add(new(trigger, _nextState));
+                    collection[trigger] = _nextState;
                 }
                 else
                 {
-                    _machine._triggerTransitions.Add(_currentState, new List<KeyValuePair<TTrigger, TState>>() { new(trigger, _nextState) });
+                    _machine._triggerTransitions.Add(_currentState, new Dictionary<TTrigger, TState>() { { trigger, _nextState } });
                 }
                 return this;
             }
